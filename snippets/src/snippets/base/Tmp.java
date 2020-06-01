984ad178074b5015e45cff48f8eee459eece4f46
zhaobingwang <zhaobingwang@foxmail.com>
2020-06-01 09:14:31 +0800

    junit单元测试

--- snippets/src/snippets/base/Tmp.java
+++ snippets/src/snippets/base/Tmp.java
@@ -4,4 +4,15 @@ public class Tmp {
     public static String SayHi() {
         return "Hi";
     }
+
+    public static long fact(long n) {
+        if (n < 0) {
+            throw new IllegalArgumentException();
+        }
+        long r = 1;
+        for (long i = 1; i <= n; i++) {
+            r = r * i;
+        }
+        return r;
+    }
 }
--- snippets/test/unittest/snippets/base/TmpTest.java
+++ snippets/test/unittest/snippets/base/TmpTest.java
@@ -1,14 +1,36 @@
 package unittest.snippets.base;
 
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import snippets.base.Tmp;
 
 public class TmpTest {
+    @Before
+    public void setUp() {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
     @Test
     public void sayHi() {
         String result = Tmp.SayHi();
         Assert.assertEquals(result, "Hi");
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNegative() {
+        Tmp.fact(-1);
+    }
+
+//    @Test(expected = IllegalArgumentException.class)
+//    public void testNegative2() {
+//        Tmp.fact(-2);
+//    }
 }
