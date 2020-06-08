622f78708a46a8cafde4b4809f56e1ae04c53022
zhaobingwang <zhaobingwang@foxmail.com>
2020-06-08 17:10:54 +0800

    spring boot 引入redis

--- spring/boot/src/main/java/com/example/demo/controller/HelloController.java
+++ spring/boot/src/main/java/com/example/demo/controller/HelloController.java
@@ -4,13 +4,20 @@ import com.example.demo.config.AppProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
 public class HelloController {
+    private final String KEY_TEST = "key_test";
     @Autowired
     private AppProperties appProperties;
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
 
     private final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
@@ -18,6 +25,12 @@ public class HelloController {
     public String hello() {
         System.out.println(appProperties.getDescription());
         logger.info("Interface hello is accessed");
-        return "hello";
+        String result = "hello";
+        if (redisTemplate.opsForValue().get(KEY_TEST) == null) {
+            redisTemplate.opsForValue().set(KEY_TEST, result + " from redis", 1, TimeUnit.MINUTES);
+        } else {
+            result = redisTemplate.opsForValue().get(KEY_TEST);
+        }
+        return result;
     }
 }
