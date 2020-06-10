d5c74f05585dc137e758876d446c31d20689a326
zhaobingwang <zhaobingwang@foxmail.com>
2020-06-10 10:18:08 +0800

    use application.yml replace application.properties

--- spring/boot/src/main/java/com/example/demo/task/TmpTask.java
+++ spring/boot/src/main/java/com/example/demo/task/TmpTask.java
@@ -1,12 +1,14 @@
 package com.example.demo.task;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class TmpTask {
     @Scheduled(cron = "${task.tmp.default.cron}")
     public void printTime() {
-        System.out.println(System.currentTimeMillis());
+        log.info(String.valueOf(System.currentTimeMillis()));
     }
 }
