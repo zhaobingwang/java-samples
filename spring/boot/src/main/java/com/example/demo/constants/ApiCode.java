18d930a8f95f039e6a96434aa3f17a96babed54f
zhaobingwang <zhaobingwang@foxmail.com>
2020-06-10 17:18:34 +0800

    API CODE

---
+++ spring/boot/src/main/java/com/example/demo/constants/ApiCode.java
@@ -0,0 +1,20 @@
+package com.example.demo.constants;
+
+public class ApiCode {
+
+    public static final String SYS_INTERNAL_SERVER_ERROR = "-9";
+    public static final String SYS_INTERNAL_SERVER_ERROR_MESSAGE = "服务器发生了未知的错误";
+
+    public static final String SYS_BUSY = "-1";
+    public static final String SYS_BUSY_MESSAGE = "系统繁忙";
+    public static final String OK = "0";
+    public static final String OK_MESSAGE = "ok";
+    /**
+     * 用户未找到
+     */
+    public static final String USER_NOT_FOUND = "40001";
+    /**
+     * 用户未找到提示信息
+     */
+    public static final String USER_NOT_FOUND_MESSAGE = "用户未找到";
+}
