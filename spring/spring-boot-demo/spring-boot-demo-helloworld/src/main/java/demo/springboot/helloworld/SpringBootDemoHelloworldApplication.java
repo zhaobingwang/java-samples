efbd2a9cdabd3ff8f7e9ec302d62659c7517eeae
zhaobingwang <zhaobingwang@foxmail.com>
2020-08-17 15:17:36 +0800

    spring-boot-demo-heloworld

--- spring/spring-boot-demo/spring-boot-demo-helloworld/src/main/java/demo/springboot/helloworld/SpringBootDemoHelloworldApplication.java
+++ spring/spring-boot-demo/spring-boot-demo-helloworld/src/main/java/demo/springboot/helloworld/SpringBootDemoHelloworldApplication.java
@@ -1,13 +1,25 @@
 package demo.springboot.helloworld;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootDemoHelloworldApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDemoHelloworldApplication.class, args);
     }
 
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(required = false, name = "who") String who) {
+        if (StringUtils.isBlank(who)) {
+            who = "World";
+        }
+        return String.format("Hello,%s!", who);
+    }
 }
