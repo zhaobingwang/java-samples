053891b559b3caa0812fcee6e5ebdc67bfac0fe8
zhaobingwang <zhaobingwang@foxmail.com>
2020-08-26 16:48:47 +0800

    spring-boot-demo-actuator

---
+++ spring/spring-boot-demo/spring-boot-demo-actuator/src/test/java/demo/springboot/actuator/SpringBootDemoActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.springboot.actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoActuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
