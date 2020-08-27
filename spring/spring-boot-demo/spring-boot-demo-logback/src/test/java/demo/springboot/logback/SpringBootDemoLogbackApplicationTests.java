2c2b50cbee7c1031c1a96dd27c577bc4e916bd38
zhaobingwang <zhaobingwang@foxmail.com>
2020-08-27 09:44:35 +0800

    spring-boot-demo-logback

---
+++ spring/spring-boot-demo/spring-boot-demo-logback/src/test/java/demo/springboot/logback/SpringBootDemoLogbackApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.springboot.logback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoLogbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
