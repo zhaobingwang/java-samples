8467adb1b4aa674c4774ceb0eb59d7575a3d4f89
zhaobingwang <zhaobingwang@foxmail.com>
2021-01-19 15:00:40 +0800

    feat: add springboot.orm.mybatis demo

--- spring/spring-boot-demo/spring-boot-demo-orm-mybatis/src/main/java/demo/springboot/orm/mybatis/SpringBootDemoOrmMybatisApplication.java
+++ spring/spring-boot-demo/spring-boot-demo-orm-mybatis/src/main/java/demo/springboot/orm/mybatis/SpringBootDemoOrmMybatisApplication.java
@@ -1,14 +1,21 @@
 package demo.springboot.orm.mybatis;
 
+import demo.springboot.orm.mybatis.mapper.UserMapper;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@MapperScan(basePackages = {"demo.springboot.orm.mybatis.mapper"})
 @SpringBootApplication
 @RestController
 public class SpringBootDemoOrmMybatisApplication {
+
+    @Autowired
+    UserMapper userMapper;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDemoOrmMybatisApplication.class, args);
@@ -16,6 +23,7 @@ public class SpringBootDemoOrmMybatisApplication {
 
     @GetMapping("/hello")
     public String sayHello(@RequestParam(required = false, name = "who") String who) {
-        return String.format("Hello,%s!", who);
+        return userMapper.selectUserById(1L).getName();
+//        return String.format("Hello,%s!", who);
     }
 }
