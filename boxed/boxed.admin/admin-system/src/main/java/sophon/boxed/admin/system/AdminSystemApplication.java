0dcc37b7aa598e677e3b45d5528753c372622089
zhaobingwang <zhaobingwang@foxmail.com>
2021-06-18 10:06:37 +0800

    initialize sophon.boxed.admin

---
+++ boxed/boxed.admin/admin-system/src/main/java/sophon/boxed/admin/system/AdminSystemApplication.java
@@ -0,0 +1,13 @@
+package sophon.boxed.admin.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminSystemApplication.class, args);
+    }
+
+}
---
+++ boxed/boxed.admin/admin-system/src/test/java/sophon/boxed/admin/system/AdminSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package sophon.boxed.admin.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
